5385901127996ac324c0991080d7adbaae9aaf83
Zak <89150707+Zak0749@users.noreply.github.com>
2021-10-04 08:05:52 +0100

    Added my controllers

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserModule } from './modules/user/user.module';
+import { SessionModule } from './modules/session/session.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/bit-todo-dev'),
+    UserModule,
+    SessionModule,
+  ],
 })
 export class AppModule {}
